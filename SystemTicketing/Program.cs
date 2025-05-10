4aae6821fc86c7804b49dd11c798ee82578e71a3
AL HAYTHAM <AL HAYTHAM@DESKTOP-4RKD8DQ>
2025-05-10 07:59:48 +0300

    Program.css update

--- SystemTicketing/Program.cs
+++ SystemTicketing/Program.cs
@@ -1,7 +1,14 @@
+using DataAccessLyer.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
